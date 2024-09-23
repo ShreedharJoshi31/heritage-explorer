215ae28e56c8943869e455cd1d51206d4266aaa7
ShreedharJoshi31 <shreedharjoshi03@gmail.com>
2024-09-23 21:54:56 +0530

    deploy frontend

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:4000/api/v1";
+// export const BASE_URL = "http://localhost:4000/api/v1";
 export const FLASK_URL = "http://localhost:5000";
-// export const BASE_URL =  'https://heritage-explorer-backend.onrender.com/api/v1'
+export const BASE_URL = "https://heritage-explorer.onrender.com/api/v1";
